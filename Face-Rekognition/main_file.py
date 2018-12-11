c792eff9c466242c6eb773ac30819d93d6200225
Santosh M N <31931121+santoshmn26@users.noreply.github.com>
2018-12-11 16:27:41 -0600

    Update main_file.py

--- Face-Rekognition/main_file.py
+++ Face-Rekognition/main_file.py
@@ -2,54 +2,56 @@ import cv2
 import boto3
 import os
 
-#---------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------
 # Capturing face for login verification
 
-def retry(login_attempt):
-    capture=cv2.VideoCapture(0)
+def attempt_login(login_attempt):
+    capture=cv2.VideoCapture(0)          # Capture input from webcam
     while(capture.isOpened()):
         ret, frame = capture.read()
         if ret:
             cv2.imshow('frame', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
-                cv2.imwrite("screenshot.jpg",frame)
+                cv2.imwrite("screenshot.jpg",frame)     # Capture a screenshot from the webcam feed when pressed 'q'
                 capture.release()
                 cv2.destroyAllWindows()
                 break
 
     try:
-        imageFile="screenshot.jpg"     # Path for the generated file
+        imageFile="screenshot.jpg"     # Path for the generated snapshot 
         with open(imageFile, 'rb') as image:
+            # Call the AWS-Rekognition service: search_faces_by_image
             response=client.search_faces_by_image(CollectionId=collectionId,
                                         Image={'Bytes': image.read()},
                                         FaceMatchThreshold=threshold,
                                         MaxFaces=maxFaces)
-            print("here")
-            return(response)
+            return(response)           # Result for the face detected
 
     except:
         print("Failed to login!")
-        os.remove("screenshot.jpg")
-        login_attempt+=1
-        if(login_attempt>4):
+        os.remove("screenshot.jpg")    # Removing the snapshot of the user
+        login_attempt+=1               # Count the user's login attempts
+        if(login_attempt>4):           # Login attempt exceeds 3 exit the program
             exit()
-        response=retry(login_attempt)
-        return(response)
+        response=attempt_login(login_attempt)  # Login failed retry again
+        return(response)               # Result for the face detected
         
-#---------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------
+# Main function
 
-collectionId='MyCollection'                  # Collection of faces stored
-threshold = 15                               # Confidence level of detection
-maxFaces=2                                   # Maximum number of faces to detect
-login_attempt=1
-client=boto3.client('rekognition')
-response=retry(login_attempt)                       
-faceMatches=response['FaceMatches']
-for match in faceMatches:
-    print ('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
-    match['Face']['ExternalImageId']=match['Face']['ExternalImageId'].split('.')
-    print('ExternalImageId:', match['Face']['ExternalImageId'][0])
+if __name__ == "__main__":
+    collectionId='MyCollection'                  # Collection of faces stored
+    threshold = 15                               # Confidence level of detection
+    maxFaces=2                                   # Maximum number of faces to detect
+    login_attempt=1
+    client=boto3.client('rekognition')
+    response=attempt_login(login_attempt)                       
+    faceMatches=response['FaceMatches']
+    for match in faceMatches:
+        print ('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
+        match['Face']['ExternalImageId']=match['Face']['ExternalImageId'].split('.')
+        print('ExternalImageId:', match['Face']['ExternalImageId'][0])
+        os.remove("screenshot.jpg")             # Removing the saved snapshot of the user
+        
+#--------------------------------------------------------------------------------------------------------------------------------
 
-#---------------------------------------------------------------------
-
-os.remove("screenshot.jpg")
