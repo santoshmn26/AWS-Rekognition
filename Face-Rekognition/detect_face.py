077ac35d6cb41d840092e759f8743eb818623b22
Santosh M N <31931121+santoshmn26@users.noreply.github.com>
2018-12-12 10:46:53 -0500

    Add files via upload
    Test out the services

---
+++ Face-Rekognition/detect_face.py
@@ -0,0 +1,32 @@
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+import sys
+if __name__ == "__main__":
+
+    bucket='reko-face'
+    collectionId='MyCollection'
+    fileName=sys.argv[-1]
+    threshold = 70
+    maxFaces=2
+
+    client=boto3.client('rekognition')
+
+  
+    response=client.search_faces_by_image(CollectionId=collectionId,
+                                Image={'S3Object':{'Bucket':bucket,'Name':fileName}},
+                                FaceMatchThreshold=threshold,
+                                MaxFaces=maxFaces)
+
+                                
+    faceMatches=response['FaceMatches']
+    print ('Matching faces')
+    for match in faceMatches:
+            print ('FaceId:' + match['Face']['FaceId'])
+            print ('Similarity: ' + "{:.2f}".format(match['Similarity']) + "%")
+            match['Face']['ExternalImageId']=match['Face']['ExternalImageId'].split('.')
+            print('ExternalImageId:', match['Face']['ExternalImageId'][0])
+            
+            
+
