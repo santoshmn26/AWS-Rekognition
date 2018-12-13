a24bdc7c7f949783592d3bfc5f655c8cbc71d054
Santosh M N <31931121+santoshmn26@users.noreply.github.com>
2018-12-13 09:35:04 -0600

    Update upload_to_s3.py

--- Face-Rekognition/upload_to_s3.py
+++ Face-Rekognition/upload_to_s3.py
@@ -1,7 +1,7 @@
 import boto3
 import sys
 s3 = boto3.client('s3')
-bucket = 'reko-face'
-file_name = sys.argv[1]                 # First argument to pass the path of the file
+bucket = 'reko-face'                # Bucket name for storing the image in s3
+file_name = sys.argv[1]             # First argument to pass the path of the file
 key_name = sys.argv[2]              # Second argument to pass the name of the file to be stored in s3
-s3.upload_file(file_name, bucket, key_name)
+s3.upload_file(file_name, bucket, key_name)  # upload_file API call to store the image in s3
