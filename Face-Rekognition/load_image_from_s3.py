cab3ec66a0b8f32b84170c9d25c44619a1f9a323
Santosh M N <31931121+santoshmn26@users.noreply.github.com>
2018-12-13 10:03:12 -0600

    Update load_image_from_s3.py

--- Face-Rekognition/load_image_from_s3.py
+++ Face-Rekognition/load_image_from_s3.py
@@ -1,11 +1,13 @@
 import boto3
+import sys
 
 if __name__ == "__main__":
 
-    imageFile="image.jpg"       # image file to detect labels
+    imageFile=sys.argv[1]       # image file to detect labels
+    bucket=sys.argv[2]
     client=boto3.client('rekognition')
     
-    response = client.detect_labels(Image=imageFile)
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':imageFile}})
         
     print('Detected labels in ' + imageFile)    
     for label in response['Labels']:
